6044f1af290c00ffcd96529cab2cfdaa7bd7dde8
unrealbootpower <46008343+unrealbootpower@users.noreply.github.com>
2020-08-24 21:43:21 +0200

    inizio nascondino

---
+++ scouts - Copy/Assets/Scripts/playerNascondino.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerNascondino : MonoBehaviour
+{
+    public Joystick joy;
+    public float playSpeed = 40f;
+    private void FixedUpdate()
+    {
+        Vector3 movement = joy.direction;
+        transform.position = Vector3.Lerp(transform.position, transform.position + movement, Time.deltaTime*playSpeed);
+    }
+}
