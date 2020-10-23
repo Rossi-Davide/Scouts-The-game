9998e26ce838917c101309ddd40c978a5c8676fd
unrealbootpower <46008343+unrealbootpower@users.noreply.github.com>
2020-10-23 18:46:07 +0200

    arredamento labirinto, sistema danni

---
+++ scouts - Copy/Assets/LIfeNascondino.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LIfeNascondino : MonoBehaviour
+{
+    public float life = 100f,spillo=5f,dinamite=10f;
+    public Vector2 force;
+    labirintoManager lb;
+    Rigidbody2D rb;
+    public void Start()
+    {
+        lb = GameObject.Find("/GameManager").GetComponent<labirintoManager>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    public void Spillo()
+    {
+        life -= spillo;
+        rb.AddForce(force);
+        if (life <= 0)
+        {
+            lb.StartCoroutine("Sconfitta");
+        }
+    }
+
+    public void Dinamite()
+    {
+        life -= dinamite;
+        if (life <= 0)
+        {
+            lb.StartCoroutine("Sconfitta");
+        }
+    }
+
+
+}
---
+++ scouts - Copy/Assets/dinamite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dinamite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Player")
+        {
+            Collider2D cl = collision.collider;
+            cl.GetComponent<LIfeNascondino>().Dinamite();
+        }
+    }
+}
