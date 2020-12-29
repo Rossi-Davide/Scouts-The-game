78068fa624a05a1e90a9345fc373a05f6546e5c8
unrealbootpower <46008343+unrealbootpower@users.noreply.github.com>
2020-12-29 18:01:22 +0100

    scus non ho fatto pull

---
+++ scouts - Copy/Assets/controlAnimRefettorio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlAnimRefettorio : MonoBehaviour
+{
+    public GameObject col1, col2, col3;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CheckAnim",0.1f, 0.5f);
+    }
+
+    void CheckAnim()
+    {
+        
+    }
+}
