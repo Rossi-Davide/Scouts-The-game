10fc9b86c1f2dd356b52ae18af99b3a2a266301a
Squier2264 <saracappelli22@gmail.com>
2021-02-20 17:29:16 +0100

    lame

--- scouts - Copy/Assets/Scripts/PlayerAction.cs
+++ scouts - Copy/Assets/Scripts/PlayerAction.cs
@@ -9,6 +9,7 @@ public class PlayerAction : ScriptableObject
     public Condition[] conditions;
     public bool hasInfoPanel;
     public bool showInActionList;
+    public AnimationRoutine routine;
     //public BuildingState state;
 
     public int timeNeeded;
