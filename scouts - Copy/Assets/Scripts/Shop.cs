d0fb7e759472432edc6f67a3dbd46da900425afc
Squier2264 <saracappelli22@gmail.com>
2021-03-27 16:28:44 +0100

    ~3

--- scouts - Copy/Assets/Scripts/Shop.cs
+++ scouts - Copy/Assets/Scripts/Shop.cs
@@ -219,7 +219,7 @@ public class Shop : MonoBehaviour
 		//	else index = o.level - 1;
 		//}
 		//else index = o.level;
-		index = canIncreaseLevel ? o.level + 1 : o.level;
+		index = o.exists ? (canIncreaseLevel ? o.level + 1 : o.level) : o.level;
 
 		pt = o.shopInfos[index].priceCounter;
 		pc = o.shopInfos[index].Price;
