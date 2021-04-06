b29a79abd0e8f5040ec9f456ba7d41a9b29501e0
unrealbootpower <46008343+unrealbootpower@users.noreply.github.com>
2021-04-06 12:34:15 +0200

    grandi cose

---
+++ scouts - Copy/Assets/Scripts/nameOfSq.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class nameOfSq : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+        gameObject.GetComponent<TextMeshProUGUI>().text = "Squadriglia: "+Player.instance.squadriglia.name;
+    }
+
+    
+}
